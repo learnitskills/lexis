d6c6380eaed5d2497e52d2635e8e90580f3868e4
learnitskills <64744608+learnitskills@users.noreply.github.com>
2022-07-16 11:01:16 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,4 +1,11 @@
-
-output "bucket-domain-op" {
+output "bucket_output" {
   value = aws_s3_bucket.mybucket
+}
+
+output "zone_output" {
+  value = aws_route53_zone.zone
+}
+
+output "s3_config_output" {
+  value = aws_s3_bucket_website_configuration.webhosting
 }

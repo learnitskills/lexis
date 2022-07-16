031036045938770715e3e5e9ed46620254b6d2fe
learnitskills <64744608+learnitskills@users.noreply.github.com>
2022-07-16 11:01:57 +0530

    Update route53.tf

--- route53.tf
+++ route53.tf
@@ -1,15 +1,15 @@
-resource "aws_route53_zone" "hosted_zone" {
+resource "aws_route53_zone" "zone" {
   name = var.bucket_name
 }
 
 
-resource "aws_route53_record" "hosted_zone_record" {
-  zone_id = aws_route53_zone.hosted_zone.zone_id
+resource "aws_route53_record" "record" {
+  zone_id = aws_route53_zone.zone.zone_id
   name    = var.bucket_name
   type    = "A"
   alias {
-    name                   = aws_s3_bucket.mybucket.website_domain
     zone_id                = aws_s3_bucket.mybucket.hosted_zone_id
+    name                   = aws_s3_bucket_website_configuration.webhosting.website_domain
     evaluate_target_health = true
   }
 }
